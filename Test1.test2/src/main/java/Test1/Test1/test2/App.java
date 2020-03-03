ea5356972e5937fa1f70a895fbf7f5e932f6e086
Vivek_CG <vivek.cg@abc.com>
2020-03-03 19:25:11 +0530

    First commit from CG

---
+++ Test1.test2/src/main/java/Test1/Test1/test2/App.java
@@ -0,0 +1,13 @@
+package Test1.Test1.test2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Test1.test2/src/test/java/Test1/Test1/test2/AppTest.java
@@ -0,0 +1,20 @@
+package Test1.Test1.test2;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
